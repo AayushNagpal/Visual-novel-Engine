a62bd7b071bc93ebbc7421dcca6028b3b1a6cf9b
hckadamb <hckadamb@uno.edu>
2016-04-06 12:13:27 -0500

    script changes and the game activities

---
+++ src/VisualNovelEngine/util/ScriptLine.java
@@ -0,0 +1,66 @@
+package enders.visual.novel;
+
+public class ScriptLine
+{
+  private String action;
+  private String name;
+  private String src;
+  private String type;
+  private String value;
+  
+  public String getAction()
+  {
+    return this.action;
+  }
+  
+  public String getName()
+  {
+    return this.name;
+  }
+  
+  public String getSrc()
+  {
+    return this.src;
+  }
+  
+  public String getType()
+  {
+    return this.type;
+  }
+  
+  public String getValue()
+  {
+    return this.value;
+  }
+  
+  public void setAction(String paramString)
+  {
+    this.action = paramString;
+  }
+  
+  public void setName(String paramString)
+  {
+    this.name = paramString;
+  }
+  
+  public void setSrc(String paramString)
+  {
+    this.src = paramString;
+  }
+  
+  public void setType(String paramString)
+  {
+    this.type = paramString;
+  }
+  
+  public void setValue(String paramString)
+  {
+    this.value = paramString;
+  }
+}
+
+
+/* Location:              C:\Users\Harry\Desktop\CS-Details\Introduction to Software Engineering\new-project\dex2jar-0.0.9.15\dex2jar-0.0.9.15\classes-dex2jar.jar!\enders\visual\novel\ScriptLine.class
+ * Java compiler version: 6 (50.0)
+ * JD-Core Version:       0.7.1
+ */
