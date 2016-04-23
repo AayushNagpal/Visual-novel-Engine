df527cbbc7f6929abae76fee8e7d8e8a7f80b07c
Aayush <anagpal@uno.edu>
2016-04-23 22:47:29 +0000

    Replace StartingActivity.java with Start.java

--- src/VisualNovelEngine/src/com/csci5210/se/visualnovelengine/StartingActivity.java
+++ src/VisualNovelEngine/src/com/csci5210/se/visualnovelengine/StartingActivity.java
@@ -1,24 +1,59 @@
-package com.example.test;
 
+package com.csci5210.se.visualnovelengine;
+
+import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
-import android.view.MotionEvent;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class StartingActivity extends Activity {
+public class Start extends Activity {
+
+	public static boolean mute;
+	public static MenuItem[] items;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.first_xml);
-	}
-	
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		// TODO Auto-generated method stub
-		Intent MM = new Intent(this, MainActivity.class);
-		startActivity(MM);
-		return super.onTouchEvent(event);
+		setContentView(R.layout.activity_start);
+
+		/* Set background, if exists */
+		int id = getResources().getIdentifier("drawable/start", null, getPackageName());
+		if(id > 0)
+			findViewById(R.id.relativeLayout1).setBackgroundResource(id);
+
+		/* Gathering text */
+		ListView listView = (ListView)findViewById(R.id.listView1);
+		String[] menu = getResources().getStringArray(R.array.start);
 		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu);
+		listView.setAdapter(adapter);
+		
+		/* Initialization */
+		mute = false;
+
+		/* Listener */
+		listView.setOnItemClickListener(new OnItemClickListener() {
+	        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	        	Intent intent;
+		        switch(position) {
+			       
+			        case 0: // new game
+			        	intent = new Intent(Start.this, Game.class);
+			        	intent.putExtra("mute", mute);
+			        	intent.putExtra("script", "");
+			    		Start.this.startActivity(intent);
+			        	break;
+			        case 1: // quit
+			    		Start.this.finish();
+			        	break;
+		        }
+	        }
+	    });
 	}
 }
